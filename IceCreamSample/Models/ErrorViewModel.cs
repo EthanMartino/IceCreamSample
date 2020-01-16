2560aad3e8a78e1d44857b89dbba7f704e6649d6
EthanMartino <martino9448@student.cptc.edu>
2020-01-16 15:26:11 -0800

    Add project files.

---
+++ IceCreamSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IceCreamSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
