4ac84ecd5e9809e31d2d707c3faf87e8c7d25ddc
EthanMartino <martino9448@student.cptc.edu>
2020-01-20 20:45:01 -0800

    Created the IceCream class

---
+++ IceCreamSample/IceCream.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCreamSample
+{
+    public class IceCream
+    {
+        /// <summary>
+        /// The flavor of the Ice Cream
+        /// </summary>
+        string Flavor { get; set; }
+
+        /// <summary>
+        /// The number of scoops of Ice Cream
+        /// </summary>
+        byte Scoops { get; set; }
+
+        /// <summary>
+        /// The type of container for the Ice Cream (Cone or Cup)
+        /// </summary>
+        string Container { get; set; }
+    }
+}
