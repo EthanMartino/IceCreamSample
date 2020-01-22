47298164592b575ad93f36ee0731386b6a036288
EthanMartino <martino9448@student.cptc.edu>
2020-01-21 16:39:19 -0800

    Connected IceCream objects to IceCream Database

---
+++ IceCreamSample/DBHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCreamSample
+{
+    public class DBHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            string con = "Data Source=localhost;Initial Catalog=IceCream;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            return new SqlConnection(con);
+        }
+    }
+}
---
+++ IceCreamSample/IceCreamDB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCreamSample
+{
+    public class IceCreamDB
+    {
+        public static void Add(IceCream ice)
+        {
+            //Establish Connection
+            SqlConnection con = DBHelper.GetConnection();
+
+            //Set Up Queries
+            SqlCommand addCmd = new SqlCommand();
+            addCmd.Connection = con;
+            addCmd.CommandText =
+                "INSERT INTO IceCream(Flavor, Scoops, Container)" +
+                    "VALUES(@Flavor, @Scoops, @Container)";
+            addCmd.Parameters.AddWithValue("@Flavor", ice.Flavor);
+            addCmd.Parameters.AddWithValue("@Scoops", ice.Scoops);
+            addCmd.Parameters.AddWithValue("@Container", ice.Container);
+
+            //Open connection, Execute, and finally Dispose of connection
+            try
+            {
+                con.Open();
+                addCmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                con.Dispose();
+            }
+        }
+    }
+}
