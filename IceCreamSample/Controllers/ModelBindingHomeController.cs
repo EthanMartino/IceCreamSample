94fd05842ca22862c16121af8ffddd00f1cdd321
EthanMartino <martino9448@student.cptc.edu>
2020-01-21 16:25:13 -0800

    Created Second Form and used model binding t create an IceCream object

---
+++ IceCreamSample/Controllers/ModelBindingHomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IceCreamSample.Controllers
+{
+    public class ModelBindingHomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IceCream ice)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: add to db
+                return RedirectToAction("Index", "Success");
+            }
+            else
+            {
+                return View(ice);
+            }
+        }
+    }
+}
